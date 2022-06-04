b75f928e895cd7548d7c8542725aca56d62011f7
ShajiahSiddiqah <shajiah.siddiqah36@gmail.com>
2022-06-04 20:34:22 +0500

    New program edit

---
+++ AdditionProgram.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number1;
+    cout << "Enter first number:- ";
+    cin >> number1;
+    int number2;
+    cout << "enter second number:- ";
+    cin >> number2;
+    cout << "The sum is " << number1 + number2 << endl;
+
+
+
+    return 0;
+}
