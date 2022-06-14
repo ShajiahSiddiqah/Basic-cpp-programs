fe18619dd526041eeef8d6dfed729d2c62abbcae
ShajiahSiddiqah <shajiah.siddiqah36@gmail.com>
2022-06-15 02:06:04 +0500

    Reverse String

---
+++ reverseString.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string abc = "Samnar pagal";
+    string reverse = "";
+    int count = 0;
+     for (int i = abc.length(); i >= count; i--)
+     {
+         reverse += abc[i];
+     }
+         cout << reverse << endl;
+    
+
+    return 0;
+}
